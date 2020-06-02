2049ae6953ebe63c000f30c24f07f4395ec373c2
ankitkumar9018 <ankitkumar.9018@gmail.com>
2020-06-02 23:10:17 +0200

    Update finalProject.py

--- finalProject.py
+++ finalProject.py
@@ -3,86 +3,15 @@
 Created on Tue May 26 20:18:22 2020
 
 @author: ankit
-
 duplicates primary 1, 3770
 """
 
 
 #%%
-#import dbfread
-#import numpy as np
-#import datetime
-##for record in dbfread.open('D:/converter/PRO_DRU_Clip.DBF'):
-##    print(record)
-#    
-##test = dbfread.open('D:/converter/SUCHPRO.DBF')
-#test = dbfread.open('G:/work/code/converter/project/SUCHPRO.DBF')
-#rawData = test[0].keys()
-#headerList =[]
-#notizList = []
-#notizListComplete = []
-#
-#for header in rawData:
-#    headerList.append(header)
-#
-#notiz = headerList[31]
-#
-#for record in test:
-#    notizListComplete.append(record[notiz])
-#    if record[notiz] is not None:
-#        notizList.append(record[notiz])
-
-    
-#dbt =  dbfread.memo.open_memofile('D:/converter/SUCHPRO.DBT',1)
-#dbtFile =  dbfread.memo.open_memofile('D:/converter/SUCHPRO.DBT',1)
-        
-        
-#%%
-#i = 1
-#newList = []
-#indexList = []
-#ind = 0
-#for record in test:
-#    if record['NUMMER'] is not None:
-#        if record['NUMMER'] != i:
-#            newList.append(record['NUMMER'])
-#            indexList.append(ind)
-#            i = record['NUMMER']
-#        i = i + 1
-#    ind = ind + 1
-
-
-#%%
-#import csv
-#import sys
-#table = dbfread.DBF('D:/converter/SUCHPRO.DBF')
-##writer = csv.writer(sys.stdout)
-##
-##writer.writerow(table.field_names)
-##for record in table:
-##    writer.writerow(list(record.values()))
-#    
-#
-#f = open('D:/converter/test.csv', 'w')
-#
-#with f:
-#
-#    writer = csv.writer(f)
-#    
-#    for row in table:
-#        writer.writerow(list(record.values()))
-
-
-
-#%%  making a sql table from dbf
-        
 import dbfread
 import numpy as np
 import datetime
-#for record in dbfread.open('D:/converter/PRO_DRU_Clip.DBF'):
-#    print(record)
-    
-#test = dbfread.open('D:/converter/SUCHPRO.DBF')
+
 test = dbfread.open('G:/work/code/converter/project/SUCHPRO.DBF')
 rawData = test[0].keys()
 test.pop(0)
@@ -97,7 +26,6 @@ typesList = []
 #%%
 
 import pandas as pd
-
 data = pd.read_excel ('G:/work/code/converter/project/project.XLS') 
 df = pd.DataFrame(data, columns= ["Unnamed: 31"])
 print (df)
@@ -105,7 +33,6 @@ notizList = df.values.tolist()
 
 
 #%%
-
 for header in rawData:
     headerList.append(header)
 
@@ -125,7 +52,6 @@ for values in test:
     localIndex = localIndex + 1
        
 #%%
-
 lengthOfList = len(tableList)
 index = 0 
   
@@ -147,16 +73,13 @@ while lengthOfList > 0 and ntype == True:
 
     
 #%%
-
 import mysql.connector as mysql
 
-## connecting to the database using 'connect()' method
-## it takes 3 required parameters 'host', 'user', 'passwd'
 db = mysql.connect(
     host = "localhost",
     user = "root",
-    passwd = "ankitRaj21!",
-    database = "ute"
+    passwd = "password",
+    database = "dbname"
 )
 
 print(db) # it will print a connection object if everything is fine
@@ -166,20 +89,6 @@ cursor = db.cursor()
 
 
 #%% switch case
-
-
-#def week(i):
-#    switcher={
-#            0:'Sunday',
-#            1:'Monday',
-#            2:'Tuesday',
-#            3:'Wednesday',
-#            4:'Thursday',
-#            5:'Friday',
-#            6:'Saturday'
-#             }
-#    return switcher.get(i,"Invalid day of week")
-
 def typeSwitch(i):
     switcher={
           int:"BIGINT",
@@ -195,8 +104,8 @@ datetime.date:"DATE",
 db = mysql.connect(
     host = "localhost",
     user = "root",
-    passwd = "ankitRaj21!",
-    database = "ute"
+    passwd = "password",
+    database = "dbname"
 )
 
 cursor = db.cursor()
@@ -249,16 +158,6 @@ for header in headerList:
 
 insertQueryFinal = insertQueryFinal + " (" + insertQueryHeaderName + ") VALUES (" + insertQueryValueName + ")"
 
-#query = "INSERT INTO users (name, user_name) VALUES (%s, %s)"
-## storing values in a variable
-#values = [
-#    ("Peter", "peter"),
-#    ("Amy", "amy"),
-#    ("Michael", "michael"),
-#    ("Hennah", "hennah")
-#]
-
-#tableList.pop(0)
 
 ## executing the query with values
 cursor.executemany(insertQueryFinal, tableList)
